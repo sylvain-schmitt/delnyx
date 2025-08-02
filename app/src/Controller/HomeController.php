a8be7ed5c9ce527714710836ce1f49db34449c91
Sylvain Schmitt <devil.9068@gmail.com>
2025-08-02 17:31:34 +0200

    Nettoyage : ignorer les fichiers Docker/infra locale + maj templates

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -11,6 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
