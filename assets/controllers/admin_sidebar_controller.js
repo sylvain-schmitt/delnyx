cd46c43d6a07d995c3bf77e41b8940f59e9ac071
sylvain.schmitt70@gmail.com <devil.9068@gmail.com>
2025-10-05 03:41:10 +0200

    debug: add console logs to admin sidebar controller
    - Add detailed logging to track controller connection
    - Log target detection (sidebar, overlay)
    - Log toggle/close method calls
    - Log class manipulation for debugging
    - Help identify where the mobile menu fails

--- assets/controllers/admin_sidebar_controller.js
+++ assets/controllers/admin_sidebar_controller.js
@@ -10,6 +10,8 @@ export default class extends Controller {
 
     connect() {
         console.log("Admin sidebar controller connected")
+        console.log("Has sidebar target:", this.hasSidebarTarget)
+        console.log("Has overlay target:", this.hasOverlayTarget)
         this.openValue = false
         this.setupEventListeners()
     }
@@ -79,7 +81,9 @@ export default class extends Controller {
      * Toggle la sidebar (mobile)
      */
     toggle() {
+        console.log("Toggle called, current openValue:", this.openValue)
         this.openValue = !this.openValue
+        console.log("New openValue:", this.openValue)
         this.updateSidebar()
     }
 
@@ -95,9 +99,12 @@ export default class extends Controller {
      * Met à jour l'état de la sidebar
      */
     updateSidebar() {
+        console.log("UpdateSidebar called, openValue:", this.openValue)
         if (this.openValue) {
+            console.log("Opening sidebar...")
             this.open()
         } else {
+            console.log("Closing sidebar...")
             this.closeSidebar()
         }
     }
@@ -106,14 +113,21 @@ export default class extends Controller {
      * Ouvre la sidebar
      */
     open() {
+        console.log("Open method called")
         if (this.hasSidebarTarget) {
+            console.log("Sidebar target found, applying classes")
             this.sidebarTarget.classList.remove('-translate-x-full')
             this.sidebarTarget.classList.add('translate-x-0')
+        } else {
+            console.log("No sidebar target found!")
         }
         
         // Afficher l'overlay sur mobile
         if (this.hasOverlayTarget) {
+            console.log("Overlay target found, showing overlay")
             this.overlayTarget.classList.remove('hidden')
+        } else {
+            console.log("No overlay target found!")
         }
         
         // Empêcher le scroll du body
@@ -124,14 +138,21 @@ export default class extends Controller {
      * Ferme la sidebar
      */
     closeSidebar() {
+        console.log("CloseSidebar method called")
         if (this.hasSidebarTarget) {
+            console.log("Sidebar target found, applying close classes")
             this.sidebarTarget.classList.add('-translate-x-full')
             this.sidebarTarget.classList.remove('translate-x-0')
+        } else {
+            console.log("No sidebar target found!")
         }
         
         // Masquer l'overlay
         if (this.hasOverlayTarget) {
+            console.log("Overlay target found, hiding overlay")
             this.overlayTarget.classList.add('hidden')
+        } else {
+            console.log("No overlay target found!")
         }
         
         // Restaurer le scroll du body
