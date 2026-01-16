b1a32889f788647c39b141734395b8b9034829c9
sylvain.schmitt70@gmail.com <devil.9068@gmail.com>
2026-01-16 18:18:26 +0100

    fix(tailwind): add explicit safelist and fix chart container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        "./src/**/*.php",
+    ],
+    safelist: [
+        'h-80',
+        'text-violet-400',
+        'w-6', 'h-6',
+        'relative',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
